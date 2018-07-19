14aa0607ef32511c01d74cf5637072785aaf1d09
WOLFCLAW <frostwolfclaw@gmail.com>
2018-07-19 17:44:41 +0900

    update docs for new tabs

---
+++ static/js/docs-layout-index.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["docs-layout-index"],{
+
+/***/ "./docs/layout/index.mdx":
+/*!*******************************!*\
+  !*** ./docs/layout/index.mdx ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _mdx_js_tag__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @mdx-js/tag */ \"./node_modules/@mdx-js/tag/dist/index.js\");\n/* harmony import */ var _mdx_js_tag__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_mdx_js_tag__WEBPACK_IMPORTED_MODULE_1__);\nvar _jsxFileName = \"/Users/sung/Develop/visits-style/docs/layout/index.mdx\";\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (function (_ref) {\n  var components = _ref.components;\n  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_mdx_js_tag__WEBPACK_IMPORTED_MODULE_1__[\"MDXTag\"], {\n    name: \"wrapper\",\n    components: components,\n    __source: {\n      fileName: _jsxFileName,\n      lineNumber: 6\n    },\n    __self: this\n  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_mdx_js_tag__WEBPACK_IMPORTED_MODULE_1__[\"MDXTag\"], {\n    name: \"p\",\n    components: components,\n    __source: {\n      fileName: _jsxFileName,\n      lineNumber: 6\n    },\n    __self: this\n  }, \"\\u753B\\u9762\\u306E\\u69CB\\u6210\\u306E\\u30EC\\u30A4\\u30A2\\u30A6\\u30C8\\u3092\\u62C5\\u5F53\\u3057\\u307E\\u3059\"));\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9kb2NzL2xheW91dC9pbmRleC5tZHguanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vZG9jcy9sYXlvdXQvaW5kZXgubWR4PzQxNjAiXSwic291cmNlc0NvbnRlbnQiOlsiXG4gIGltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcbiAgaW1wb3J0IHsgTURYVGFnIH0gZnJvbSAnQG1keC1qcy90YWcnXG4gIFxuXG5leHBvcnQgZGVmYXVsdCAoe2NvbXBvbmVudHN9KSA9PiA8TURYVGFnIG5hbWU9XCJ3cmFwcGVyXCIgIGNvbXBvbmVudHM9e2NvbXBvbmVudHN9PjxNRFhUYWcgbmFtZT1cInBcIiBjb21wb25lbnRzPXtjb21wb25lbnRzfT57YOeUu+mdouOBruani+aIkOOBruODrOOCpOOCouOCpuODiOOCkuaLheW9k+OBl+OBvuOBmWB9PC9NRFhUYWc+PC9NRFhUYWc+XG4gICJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQ0E7QUFDQTtBQUdBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUEiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./docs/layout/index.mdx\n");
+
+/***/ })
+
+}]);
