593e10744dd8ce13ef10d2737bb0358b876b21e0
WOLFCLAW <frostwolfclaw@gmail.com>
2018-10-03 16:45:10 +0900

    :sparkle: add feature to toasts - add positions for toasts - change animation for each toast items for better transitions - optimize performance for better animation

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,12 +1,12 @@
 import React, { Component, PureComponent } from 'react';
 import { createPortal } from 'react-dom';
-import Transition from 'react-transition-group/Transition';
+import CSSTransition from 'react-transition-group/CSSTransition';
 import TransitionGroup from 'react-transition-group/TransitionGroup';
-import anime from 'animejs';
 import styled, { ColorType } from '../../styled';
-import { dispatchAnimeDone, addAnimeListener } from '../../utils/anime';
 
 import Box from '../Box';
+
+type PositionType = 'top' | 'top-left' | 'top-right' | 'bottom' | 'bottom-left' | 'bottom-right';
 
 interface ToastType {
   id: string;
@@ -25,28 +25,30 @@ const Wrapper = styled(Box)`
   padding: 0.375em 0.75em;
   max-width: 100%;
   margin-bottom: 1rem;
+  z-index: 9999;
   width: fit-content;
+
+  transition-property: transform, opacity;
+  transition-timing-function: cubic-bezier(0.645, 0.045, 0.355, 1);
+  transition-duration: 250ms;
+
+  &.move-enter {
+    opacity: 0.01;
+    transform: scale(0.8);
+  }
+  &.move-enter-active {
+    opacity: 1;
+    transform: scale(1);
+  }
+  &.move-exit {
+    opacity: 1;
+    transform: scale(1);
+  }
+  &.move-exit-active {
+    opacity: 0.01;
+    transform: scale(0.8);
+  }
 `;
-
-function animeToastIn(toast: HTMLElement) {
-  anime({
-    targets: toast,
-    translateX: ['100%', 0],
-    complete: () => dispatchAnimeDone(toast),
-    easing: [0.645, 0.045, 0.355, 1],
-    duration: 250,
-  });
-}
-
-function animeToastOut(toast: HTMLElement) {
-  anime({
-    targets: toast,
-    translateX: [0, '100%'],
-    complete: () => dispatchAnimeDone(toast),
-    easing: [0.645, 0.045, 0.355, 1],
-    duration: 250,
-  });
-}
 
 export class Toast extends PureComponent<ToastProps> {
   static defaultProps = {
@@ -54,17 +56,11 @@ export class Toast extends PureComponent<ToastProps> {
   };
 
   componentDidMount() {
-    if (this.props.duration) {
-      this.timer = setTimeout(() => {
-        this.props.clear();
-      }, this.props.duration);
-    }
+    setTimeout(this.props.clear, this.props.duration);
   }
 
-  timer: any;
-
   render() {
-    const { message, color } = this.props;
+    const { message, color, id } = this.props;
     return (
       <Wrapper borderless color={color}>
         {message}
@@ -73,26 +69,58 @@ export class Toast extends PureComponent<ToastProps> {
   }
 }
 
+function setPosition(position: string) {
+  switch (position) {
+    case 'bottom': {
+      return 'bottom: 1rem; left: 50%; align-item: center;';
+    }
+    case 'bottom-left': {
+      return 'bottom: 1rem; left: 1rem; align-item: flex-start;';
+    }
+    case 'bottom-right': {
+      return 'bottom: 1rem; right: 1rem; align-item: flex-end;';
+    }
+
+    case 'top': {
+      return 'top: 1rem; left: 50%; align-items: center;';
+    }
+    case 'top-left': {
+      return 'top: 1rem; left: 1rem; align-items: flex-start;';
+    }
+    case 'top-right':
+    default: {
+      return 'top: 1rem; right: 1rem; align-items: flex-end;';
+    }
+  }
+}
+
 interface ContainerProps {
   toasts: ToastType[];
   clear: (id: string) => void;
+  position?: PositionType;
 }
 
 export default class ToastContainer extends Component<ContainerProps> {
   static defaultProps = {
     toasts: [],
+    position: 'top-right',
   };
 
   constructor(props: ContainerProps) {
     super(props);
     this.element = document.createElement('div');
     // tslint:disable-next-line
-    this.element.style.cssText = 'position: fixed; top: 1rem; right: 1rem; z-index: 9999; display: flex; flex-direction: column; align-items: flex-end;';
+    this.element.style.cssText = 'position: absolute; z-index: 9999; display: flex; flex-direction: column;';
+    this.element.style.cssText += setPosition(props.position!);
     document.body.appendChild(this.element);
   }
 
+  shouldComponentUpdate(props: ContainerProps) {
+    return props.toasts.length !== this.props.toasts.length;
+  }
+
   componentWillUnmount() {
-    if (this.element) this.element.remove();
+    if (this.element) document.body.removeChild(this.element);
   }
 
   clear = (id: string) => () => {
@@ -104,19 +132,18 @@ export default class ToastContainer extends Component<ContainerProps> {
     return (
       <TransitionGroup component={null}>
         {toasts.map(props => (
-          <Transition
+          <CSSTransition
             key={props.id}
-            addEndListener={addAnimeListener}
-            onEnter={animeToastIn}
-            onExit={animeToastOut}
             timeout={250}
+            classNames="move"
             unmountOnExit
           >
             <Toast
+              key={props.id}
               {...props}
               clear={this.clear(props.id)}
             />
-          </Transition>
+          </CSSTransition>
         ))}
       </TransitionGroup>
     );
