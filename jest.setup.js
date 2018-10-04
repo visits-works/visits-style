7b947232708a3ded65797f81b6eee495b94c1c93
WOLFCLAW <frostwolfclaw@gmail.com>
2018-10-04 11:38:32 +0900

    add simple test

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'jest-styled-components';
+const enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+// require('jest-styled-components');
 
-configure({ adapter: new Adapter() });
+enzyme.configure({ adapter: new Adapter() });
