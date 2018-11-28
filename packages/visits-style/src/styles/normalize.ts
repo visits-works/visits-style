b8136c76083de76768629079b418ee6b5b89e8b9
WOLFCLAW <frostwolfclaw@gmail.com>
2018-11-28 14:04:54 +0900

    update normalize css & font-family font-size will be theme dependent

--- packages/visits-style/src/styles/normalize.ts
+++ packages/visits-style/src/styles/normalize.ts
@@ -1,183 +1,181 @@
 import { css } from 'styled-components';
 
-/*! normalize.css v8.0.0 | MIT License | github.com/necolas/normalize.css */
+/*! based on normalize.css v8.0.0 | MIT License | github.com/necolas/normalize.css */
 export default css`
-  html {
-    display: block;
-    font-size: 16px;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    min-width: 300px;
-    overflow-x: hidden;
-    overflow-y: scroll;
-    text-rendering: optimizeLegibility;
-    text-size-adjust: 100%;
+  *, ::after, ::before {
     box-sizing: border-box;
+  }
+
+  html {
     line-height: 1.15;
-    -webkit-text-size-adjust: 100%
+    -webkit-text-size-adjust: 100%;
+    -ms-overflow-style: scrollbar;
   }
 
   body {
     margin: 0;
-    font-family: "Hiragino Sans", ヒラギノ角ゴシック, "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN W3", 游ゴシック体, "Yu Gothic", YuGothic, "ヒラギノ角ゴシック Pro", HiraKakuPro-W3, "Hiragino Kaku Gothic Pro", Quicksand, メイリオ, Meiryo, Osaka, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
-    font-size: 1rem;
-    font-weight: 400;
-    line-height: 1.5;
+    font-family: ${({ theme }: any) => theme ? theme.fontFamily : '"Hiragino Sans", ヒラギノ角ゴシック, "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN W3", 游ゴシック体, "Yu Gothic", YuGothic, "ヒラギノ角ゴシック Pro", HiraKakuPro-W3, "Hiragino Kaku Gothic Pro", Quicksand, メイリオ, Meiryo, Osaka, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif'};;
+    font-size: ${({ theme }: any) => theme ? theme.fontSize : '16px'};
+    text-align: left;
   }
 
   h1 {
-      font-size: 2em;
-      margin: .67em 0
+    font-size: 2em;
+    margin: .67em 0;
   }
 
   hr {
-      box-sizing: content-box;
-      height: 0;
-      overflow: visible
+    box-sizing: content-box;
+    height: 0;
+    overflow: visible;
   }
 
   pre {
-      font-family: monospace,monospace;
-      font-size: 1em
+    font-family: monospace,monospace;
+    font-size: 1em;
   }
 
   a {
-      background-color: transparent
+    background-color: transparent;
   }
 
   abbr[title] {
-      border-bottom: none;
-      text-decoration: underline;
-      text-decoration: underline dotted
+    border-bottom: none;
+    text-decoration: underline;
+    text-decoration: underline dotted;
   }
 
   b,strong {
-      font-weight: bolder
+    font-weight: bolder;
   }
 
   code,kbd,samp {
-      font-family: monospace,monospace;
-      font-size: 1em
+    font-family: monospace,monospace;
+    font-size: 1em;
   }
 
   small {
-      font-size: 80%
+    font-size: 80%;
   }
 
   sub,sup {
-      font-size: 75%;
-      line-height: 0;
-      position: relative;
-      vertical-align: baseline
+    font-size: 75%;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
   }
 
   sub {
-      bottom: -.25em
+    bottom: -.25em;
   }
 
   sup {
-      top: -.5em
+    top: -.5em;
   }
 
   img {
-      border-style: none
+    border-style: none;
   }
 
   button,input,optgroup,select,textarea {
-      font-family: inherit;
-      font-size: 100%;
-      line-height: 1.15;
-      margin: 0
+    font-family: inherit;
+    font-size: 100%;
+    line-height: 1.15;
+    margin: 0;
   }
 
   button,input {
-      overflow: visible
+    overflow: visible;
   }
 
   button,select {
-      text-transform: none
+    text-transform: none;
   }
 
   [type=button],[type=reset],[type=submit],button {
-      -webkit-appearance: button
+    -webkit-appearance: button;
   }
 
   [type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner {
-      border-style: none;
-      padding: 0
+    border-style: none;
+    padding: 0;
   }
 
   [type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring {
-      outline: 1px dotted ButtonText
+    outline: 1px dotted ButtonText;
+  }
+
+  input[type=date],
+  input[type=time],
+  input[type=datetime-local],
+  input[type=month] {
+    -webkit-appearance: listbox;
   }
 
   fieldset {
-      padding: .35em .75em .625em
+    padding: .35em .75em .625em;
   }
 
   legend {
-      box-sizing: border-box;
-      color: inherit;
-      display: table;
-      max-width: 100%;
-      padding: 0;
-      white-space: normal
+    box-sizing: border-box;
+    color: inherit;
+    display: table;
+    max-width: 100%;
+    padding: 0;
+    white-space: normal;
   }
 
   progress {
-      vertical-align: baseline
+    vertical-align: baseline;
+    resize: vertical;
   }
 
   textarea {
-      overflow: auto
+    overflow: auto;
   }
 
   [type=checkbox],[type=radio] {
-      box-sizing: border-box;
-      padding: 0
+    box-sizing: border-box;
+    padding: 0;
   }
 
   [type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button {
-      height: auto
+    height: auto;
   }
 
   [type=search] {
-      -webkit-appearance: textfield;
-      outline-offset: -2px
+    -webkit-appearance: textfield;
+    outline-offset: -2px;
   }
 
   [type=search]::-webkit-search-decoration {
-      -webkit-appearance: none
+    -webkit-appearance: none;
   }
 
   ::-webkit-file-upload-button {
-      -webkit-appearance: button;
-      font: inherit
+    -webkit-appearance: button;
+    font: inherit;
   }
 
   details {
-      display: block
+    display: block;
   }
 
   summary {
-      display: list-item
+    display: list-item;
   }
 
   template {
-      display: none
+    display: none;
   }
 
   [hidden] {
-      display: none
+    display: none;
   }
 
   blockquote, body, dd, dl, dt, fieldset, figure, h1, h2, h3, h4, h5, h6, hr, html, iframe, legend, li, ol, p, pre, textarea, ul {
     margin: 0;
     padding: 0;
-  }
-
-  *, ::after, ::before {
-    box-sizing: inherit;
   }
 
   button {
@@ -190,7 +188,8 @@ export default css`
   }
 
   input[type="checkbox"], input[type="radio"] {
-    vertical-align: baseline;
+    box-sizing: border-box;
+    padding: 0;
   }
 
   a {
@@ -204,8 +203,13 @@ export default css`
   }
 
   img {
-    height: auto;
-    max-width: 100%;
+    vertical-align: middle;
+    border-style: none;
+  }
+
+  svg {
+    overflow: hidden;
+    vertical-align: middle;
   }
 
   small {
@@ -227,9 +231,8 @@ export default css`
     border-spacing: 0;
   }
 
-  table td,table th {
-    text-align: left;
-    vertical-align: top;
+  th {
+    text-align: inherit;
   }
 
   ul {
