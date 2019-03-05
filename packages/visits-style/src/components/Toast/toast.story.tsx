b5485bc06ece00bb0c1deef8f6588f0bef441612
WOLFCLAW <frostwolfclaw@gmail.com>
2019-03-05 17:54:04 +0900

    add command for deploying storybook to github page

--- packages/visits-style/src/components/Toast/toast.story.tsx
+++ packages/visits-style/src/components/Toast/toast.story.tsx
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
+import { select, boolean } from '@storybook/addon-knobs';
 import Toast from '.';
 import Button from '../Button';
 
-class ToastDemo extends Component {
+class ToastDemo extends Component<{ position: string, fixed: boolean }> {
   state = { list: [], position: 'top-left' };
 
+  shouldComponentUpdate(_, state) {
+    return state.list.length !== this.state.list.length ||
+      state.position !== this.state.position;
+  }
+
   addToast = () => {
-    const colorList= ['warning', 'danger', 'info', 'primary', 'success'];
+    const colorList = ['warning', 'danger', 'info', 'primary', 'success'];
     const list = this.state.list.slice();
     const color = colorList[Math.floor(Math.random() * Math.floor(colorList.length))];
-    const id = '_' + Math.random().toString(36).substr(2, 9);
+    const id = `_${Math.random().toString(36).substr(2, 9)}`;
     if (list.length % 2) {
       // @ts-ignore
-      list.push({ id: id, color: color, message: <span>{'looooooooooooooooooooooooooooooong list number: ' + list.length}</span> });
+      list.push({
+        id, color,
+        message: <span>looooooooooooooooooooooooooooooong list number: {list.length}</span>,
+      });
     } else {
       // @ts-ignore
-      list.push({ id: id, color: color, message: 'list number: ' + list.length });
+      list.push({ id, color, message: `list number: ${list.length}` });
     }
-    this.setState({ list: list });
+    this.setState({ list });
   }
 
   clearToast = (id: string) => {
     const list = this.state.list.filter((item: any) => item.id !== id);
-    this.setState({ list: list });
+    this.setState({ list });
   }
 
-  clearAll() {
+  clearAll = () => {
     this.setState({ list: [] });
   }
 
@@ -35,19 +44,31 @@ class ToastDemo extends Component {
   }
 
   render() {
-    const positionList = ['top', 'top-left', 'top-right', 'bottom', 'bottom-left', 'bottom-right'];
-    const { list, position } = this.state;
+    const { list } = this.state;
+    const { position, fixed } = this.props;
     return (
       <div>
         <Button color="info" onClick={this.addToast}>Toast!</Button>
         <Button color="danger" onClick={this.clearAll}>Clear All</Button>
-        <Toast toasts={list} clear={this.clearToast} position="top-left" />
+        <Toast
+          toasts={list}
+          clear={this.clearToast}
+          // @ts-ignore
+          position={position}
+          fixed={fixed}
+        />
+        <div style={{ paddingBottom: '100vh' }} />
       </div>
     );
   }
 }
 
+const positionList = ['top', 'top-left', 'top-right', 'bottom', 'bottom-left', 'bottom-right'];
+
 storiesOf('components|Toast', module)
   .add('default', () => (
-    <ToastDemo />
+    <ToastDemo
+      position={select('position', positionList, 'top-left')}
+      fixed={boolean('fixed', false)}
+    />
   ));
