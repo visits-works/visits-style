3fbeae1f0de73960860af50fd5e32ceb470bb925
WOLFCLAW <frostwolfclaw@gmail.com>
2019-05-14 11:05:36 +0900

    export switch component

--- packages/visits-style/src/components/Form/index.ts
+++ packages/visits-style/src/components/Form/index.ts
@@ -5,3 +5,4 @@ export { default as Textarea } from './Textarea';
 export { default as Checkbox } from './Checkbox';
 export { default as Select } from './Select';
 export { default as Radio } from './Radio';
+export { default as Switch } from './Switch';
