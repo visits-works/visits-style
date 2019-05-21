292958d5ae8f41d0e131b46be03fee110e027c5c
WOLFCLAW <frostwolfclaw@gmail.com>
2019-05-21 15:15:46 +0900

    :bug: media query not wokring

--- packages/visits-style/src/utils/media.ts
+++ packages/visits-style/src/utils/media.ts
@@ -6,25 +6,25 @@ interface Props {
 
 export function mediaMobile({ theme }: Props) {
   if (!theme.responsive) return '@media (max-width: 0)';
-  return `@media screen and (max-width: ${theme.mobile})`;
+  return `@media screen and (max-width: ${theme.media.mobile})`;
 }
 
 export function mediaTablet({ theme }: Props) {
   if (!theme.responsive) return '@media (max-width: 0)';
-  return `@media screen and (max-width: ${theme.tablet})`;
+  return `@media screen and (max-width: ${theme.media.tablet})`;
 }
 
 export function mediaDesktop({ theme }: Props) {
   if (!theme.responsive) return '@media (max-width: 0)';
-  return `@media screen and (max-width: ${theme.desktop})`;
+  return `@media screen and (max-width: ${theme.media.desktop})`;
 }
 
 export function mediaFullHD({ theme }: Props) {
   if (!theme.responsive) return '@media (max-width: 0)';
-  return `@media screen and (max-width: ${theme.fullhd})`;
+  return `@media screen and (max-width: ${theme.media.fullhd})`;
 }
 
 export function mediaUntilFullHD({ theme }: Props) {
   if (!theme.responsive) return '@media (max-width: 0)';
-  return `@media (min-width: ${theme.fullhd})`;
+  return `@media (min-width: ${theme.media.fullhd})`;
 }
