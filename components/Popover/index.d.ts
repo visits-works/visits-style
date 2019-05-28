7f620fd5a43be82c4e60c0e73d581258e5becd25
WOLFCLAW <frostwolfclaw@gmail.com>
2019-05-28 14:21:06 +0900

    add onOpen/onClose/disabled to Popover

--- components/Popover/index.d.ts
+++ components/Popover/index.d.ts
@@ -8,6 +8,12 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
     right?: boolean;
     /** 吹き出しが表示される場所 */
     position?: 'top-left' | 'top' | 'top-right' | 'bottom-left' | 'bottom' | 'bottom-right';
+    /** 閉じた場合のコールバック */
+    onClose?: () => void;
+    /** 開けた場合のコールバック */
+    onOpen?: () => void;
+    /** コンテンツを出さない */
+    disabled?: boolean;
 }
-export default function Popover({ position, label, children, color, ...rest }: Props): JSX.Element;
+export default function Popover({ position, label, children, color, onOpen, onClose, disabled, ...rest }: Props): JSX.Element;
 export {};
