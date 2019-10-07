714508054ba96a17188611417a31094c68add82b
nukbal <frostwolfclaw@gmail.com>
2019-10-07 17:45:58 +0900

    fix: unmount modal does not revert document scroll

--- packages/visits-style/src/hooks/useScrollFix.ts
+++ packages/visits-style/src/hooks/useScrollFix.ts
@@ -4,8 +4,12 @@ export default function useScrollFix(show?: boolean) {
   useEffect(() => {
     if (show) {
       document.body.style.overflowY = 'hidden';
-    } else if (document.querySelectorAll('[aria-modal]').length <= 1) {
-      document.body.style.overflowY = 'auto';
+      return () => {
+        if (document.querySelectorAll('[aria-modal]').length <= 1) {
+          document.body.style.overflowY = 'auto';
+        }
+      };
     }
+    return () => {};
   }, [show]);
 }
