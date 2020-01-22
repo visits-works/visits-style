42689b88113e605fad7120d082ea4f7c367a1ab5
nukbal <frostwolfclaw@gmail.com>
2020-01-22 18:50:00 +0900

    refactor: no need to set name as default module

--- packages/core/src/hooks/useIsomorphicLayoutEffect.ts
+++ packages/core/src/hooks/useIsomorphicLayoutEffect.ts
@@ -1,6 +1,4 @@
 import { useEffect, useLayoutEffect } from 'react';
 import isClient from '../utils/isClient';
 
-const useIsomorphicLayoutEffect = isClient ? useLayoutEffect : useEffect;
-
-export default useIsomorphicLayoutEffect;
+export default isClient ? useLayoutEffect : useEffect;
