098541ab1eaf08e764f580c724e51a59621157d1
nukbal <frostwolfclaw@gmail.com>
2020-05-08 11:48:18 +0900

    export portal to public

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -8,6 +8,7 @@ export { default as AppBar } from './AppBar';
 export { default as Tooltip } from './Tooltip';
 export { default as Card } from './Card';
 export { default as Popover } from './Popover';
+export { default as Portal } from './Portal';
 export { default as Modal } from './Modal';
 export { default as Toast } from './Toast';
 export { default as Spinner } from './Spinner';
