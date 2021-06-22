7710832b3a348ddf6a7dc02af66183790881f9c9
nukbal <frostwolfclaw@gmail.com>
2021-06-22 19:12:59 +0900

    deps update, fix: storybook does not work

---
+++ .storybook/preview.js
@@ -0,0 +1,35 @@
+import { addDecorator } from "@storybook/react";
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+import theme from '../packages/core/src/theme';
+import css from '../packages/core/src/styles/normalize';
+
+export const parameters = {
+  name: 'visits-style',
+  url: 'https://github.com/visits-works/visits-style',
+  hierarchySeparator: /\/|\./,
+  hierarchyRootSeparator: /\|/,
+  layout: 'centered',
+};
+
+const GlobalStyle = createGlobalStyle`
+  ${css}
+`;
+
+const containerStyle = {
+  padding: '3rem',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}
+
+addDecorator(story => (
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyle  />
+      <div style={containerStyle}>
+        {story()}
+      </div>
+    </>
+  </ThemeProvider>
+));
