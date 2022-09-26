3a08cf290136ab411148e14cc3b14fa420c912f5
nukbal <frostwolfclaw@gmail.com>
2022-09-26 17:21:46 +0900

    fix: test errors

--- packages/core/src/setupTest.ts
+++ packages/core/src/setupTest.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/extend-expect';
+global.ResizeObserver = require('resize-observer-polyfill');
 
 // for typescript error
 export default null;
