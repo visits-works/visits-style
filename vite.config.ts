86d0a9e26475719e017ddaf28f0625d19128e60b
nukbal <frostwolfclaw@gmail.com>
2023-07-03 15:18:02 +0900

    fix: docs, build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,28 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import type { UserConfig } from 'vitest';
 
 import * as pkg from './package.json';
 
+const testConfig = {
+  globals: true,
+  environment: 'jsdom',
+  include: ['./src/**/**.test.{ts,tsx}'],
+  setupFiles: ['./src/setupTest.ts'],
+} as UserConfig;
+
 export default defineConfig({
-  plugins: [react(), dts({ exclude: ['src/**/*.test.(ts|tsx)', 'src/**/*.story.tsx', 'src/setupTest.ts'] })],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ['babel-plugin-styled-components', { pure: true }],
+        ],
+      },
+    }),
+    dts({ exclude: ['src/**/*.test.(ts|tsx)', 'src/**/*.story.tsx', 'src/setupTest.ts'] }),
+  ],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
@@ -21,10 +38,7 @@ export default defineConfig({
     },
     minify: false,
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    include: ['./src/**/**.test.{ts,tsx}'],
-    setupFiles: ['./src/setupTest.ts'],
-  },
+  // これがdev環境に入った場合、babel/runtimeのエラーが発生してしまうので、test環境のみ有効にする
+  // @ts-ignore
+  test: process.env.NODE_ENV === 'test' ? testConfig : undefined,
 });
