6ea8f9090fe34c2ae46ac074245c8a1a5d6eec9e
nukbal <frostwolfclaw@gmail.com>
2025-02-04 15:29:41 +0900

    chore: cleanup

--- setupTest.ts
+++ setupTest.ts
@@ -1,8 +1,2 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/vitest';
-
-// @ts-ignore
-global.ResizeObserver = require('resize-observer-polyfill');
-
-// for typescript error
-export default null;
