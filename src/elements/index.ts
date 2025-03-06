11a6e1f5317c3a2c444f6d82de10a9b2b0a91189
nukbal <frostwolfclaw@gmail.com>
2025-03-06 15:05:15 +0900

    fix: variantButton is not exposed

--- src/elements/index.ts
+++ src/elements/index.ts
@@ -1,7 +1,7 @@
 // common
 export { default as Placeholder } from './Skeleton';
 
-export { default as Button, type ButtonProps } from './Button';
+export { default as Button, type ButtonProps, varientButton } from './Button';
 export { default as ButtonGroup } from './Button/ButtonGroup';
 export { default as Spinner, type Props as SinnerProps } from './Spinner';
 export { default as Progress, type ProgressProps } from './Progress';
