0892b180e5dd1a4f997ddb6a735ca313a6600d47
nukbal <nukbal@users.noreply.github.com>
2026-09-04 07:03:40 +0000

    deploy: 42db509effd7dbdbf9019da32a7c65e852e0bc75

---
+++ assets/spinner.story-DoTo0cJf.js
@@ -0,0 +1,10 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-DeHZSEgm.js";import{n,t as r}from"./Base-CVEPMVTs.js";function i({label:e=`Loading...`,...t}){return(0,a.jsxs)(r,{as:`svg`,classList:`fill-accent animate-spin text-primary`,"aria-label":e,"aria-hidden":`true`,width:`32`,height:`32`,viewBox:`0 0 100 101`,fill:`none`,xmlns:`http://www.w3.org/2000/svg`,...t,children:[(0,a.jsx)(`path`,{d:`M100 50.6a50 50 0 1 1-100 0 50 50 0 0 1 100 0Zm-91 0a41 41 0 1 0 82 0 41 41 0 0 0-82 0Z`,fill:`currentFill`}),(0,a.jsx)(`path`,{d:`M94 39a4.2 4.2 0 0 0 3-5.4A50 50 0 0 0 41.7 1.3c-2.4.4-3.9 2.9-3.2 5.3a5 5 0 0 0 5.6 3.5A41 41 0 0 1 88 36 5 5 0 0 0 94 39Z`,fill:`currentColor`})]})}var a;function o(){return(o=e((()=>{n(),a=t(),i.__docgenInfo={description:``,methods:[],displayName:`Spinner`,props:{label:{required:!1,tsType:{name:`string`},description:``,defaultValue:{value:`'Loading...'`,computed:!1}}},composes:[`HTMLAttributes`]}})))()}var s,c,l;function u(){return(u=e((()=>{o(),s=[``,`primary`,`info`,`link`,`success`,`warning`,`danger`,`dark`],c={title:`elements/Spinner`,component:i,tags:[`autodocs`]},l={args:{className:``},parameters:{className:{values:s}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    className: ""
+  },
+  parameters: {
+    className: {
+      values: colorList
+    }
+  }
+}`,...l.parameters?.docs?.source}}}})))()}u();export{l as base,c as default};
